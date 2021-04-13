ba664d24bfbc5717cdf68398bcc6b013bcb28d81
okamoto <>
2021-04-13 19:53:09 +0900

    migrationファイル追加

---
+++ server/laravel/database/migrations/2021_03_10_094459_create_forms_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFormsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('forms', function (Blueprint $table) {
+            $table->increments('id');
+            $table->date("date")->nullable();
+            $table->integer("presentation_score")->nullable();
+            $table->integer("operation_score")->nullable();
+            $table->string("good");
+            $table->string("improve");
+            $table->string("other");
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('forms');
+    }
+}
